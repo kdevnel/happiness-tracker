158fe7ae70eaf6376636829807e42f9ef173fd2f
kdevnel <22053773+kdevnel@users.noreply.github.com>
2021-05-04 16:54:21 +0200

    Added a timer tool

--- js/app.js
+++ js/app.js
@@ -53,3 +53,70 @@ counterGroup.addEventListener("click", (e) => {
     counterActions[action]();
   }
 });
+
+var Clock = {
+  totalSeconds: 0,
+  start: function () {
+    if (!this.interval) {
+      var self = this;
+      function pad(val) {
+        return val > 9 ? val : "0" + val;
+      }
+      this.interval = setInterval(function () {
+        self.totalSeconds += 1;
+
+        document.getElementById("min").innerHTML = pad(
+          Math.floor((self.totalSeconds / 60) % 60)
+        );
+        document.getElementById("sec").innerHTML = pad(
+          parseInt(self.totalSeconds % 60)
+        );
+      }, 1000);
+    }
+  },
+  reset: function () {
+    Clock.totalSeconds = null;
+    clearInterval(this.interval);
+    document.getElementById("min").innerHTML = "00";
+    document.getElementById("sec").innerHTML = "00";
+    delete this.interval;
+  },
+  pause: function () {
+    clearInterval(this.interval);
+    delete this.interval;
+  },
+  resume: function () {
+    this.start();
+  },
+  restart: function () {
+    this.reset();
+    Clock.start();
+  },
+};
+
+const timer = document.getElementById("timer");
+timer.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    const button = e.target;
+    button.preventDefault;
+    const action = button.id;
+    const buttonActions = {
+      startButton: () => {
+        Clock.start();
+      },
+      pauseButton: () => {
+        Clock.pause();
+      },
+      resumeButton: () => {
+        Clock.resume();
+      },
+      resetButton: () => {
+        Clock.reset();
+      },
+      restartButton: () => {
+        Clock.restart();
+      },
+    };
+    buttonActions[action]();
+  }
+});
