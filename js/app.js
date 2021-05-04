1e8d987c32adca9ba585a13418745a37d7cb5d6a
kdevnel <22053773+kdevnel@users.noreply.github.com>
2021-05-04 12:33:21 +0200

    Implemented reset button and localStorage

--- js/app.js
+++ js/app.js
@@ -1,62 +1,45 @@
-const counterGroup = document.getElementById('counterGroup');
-const counterInput = document.getElementById('counterInput');
-let counterInputValue = counterInput.getAttribute('value');
+const counterGroup = document.getElementById("counterGroup");
+const counterInput = document.getElementById("counterInput");
+const trackerStorage = window.localStorage;
+const interactionCountState = localStorage.getItem("interactionCount");
+let counterInputValue = 0;
+const minValue = counterInput.getAttribute("min");
+const maxValue = counterInput.getAttribute("max");
 
+if (!interactionCountState) {
+  trackerStorage.setItem("interactionCount", 0);
+} else {
+  counterInputValue = parseInt(interactionCountState);
+  counterInput.setAttribute("value", counterInputValue);
+}
 
-counterGroup.addEventListener('click', (e) => {
-  if (e.target.tagName === 'BUTTON') {
+counterGroup.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
     const button = e.target;
     button.preventDefault;
-    const minValue = counterInput.getAttribute('min');
-    const maxValue = counterInput.getAttribute('max');
-    if (button.getAttribute('data-type') === 'plus' && counterInputValue < maxValue) {
+    const action = button.getAttribute("data-type");
+    if (action === "plus" && counterInputValue < maxValue) {
       counterInputValue++;
-      counterInput.setAttribute('value',counterInputValue);
-    } else if (button.getAttribute('data-type') === 'minus' && counterInputValue > minValue) {
+      counterInput.setAttribute("value", counterInputValue);
+      trackerStorage.setItem("interactionCount", counterInputValue);
+    } else if (action === "minus" && counterInputValue > minValue) {
       counterInputValue--;
-      counterInput.setAttribute('value',counterInputValue);
+      counterInput.setAttribute("value", counterInputValue);
+      trackerStorage.setItem("interactionCount", counterInputValue);
     }
+    // } else if (action === "reset") {
+    //   counterInputValue = 0;
+    //   counterInput.setAttribute("value", counterInputValue);
+    //   trackerStorage.setItem("interactionCount", counterInputValue);
+    // }
+
+    const counterActions = {
+      reset: () => {
+        counterInputValue = 0;
+        counterInput.setAttribute("value", counterInputValue);
+        trackerStorage.setItem("interactionCount", counterInputValue);
+      },
+    };
+    counterActions[action];
   }
 });
-
-$('.input-number').focusin(function(){
- $(this).data('oldValue', $(this).val());
-});
-$('.input-number').change(function() {
-
-  minValue =  parseInt($(this).attr('min'));
-  maxValue =  parseInt($(this).attr('max'));
-  valueCurrent = parseInt($(this).val());
-
-  name = $(this).attr('name');
-  if(valueCurrent >= minValue) {
-      $(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
-  } else {
-      alert('Sorry, the minimum value was reached');
-      $(this).val($(this).data('oldValue'));
-  }
-  if(valueCurrent <= maxValue) {
-      $(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
-  } else {
-      alert('Sorry, the maximum value was reached');
-      $(this).val($(this).data('oldValue'));
-  }
-});
-
-$(".input-number").keydown(function (e) {
-  // Allow: backspace, delete, tab, escape, enter and .
-  if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
-       // Allow: Ctrl+A
-      (e.keyCode == 65 && e.ctrlKey === true) ||
-       // Allow: home, end, left, right
-      (e.keyCode >= 35 && e.keyCode <= 39)) {
-           // let it happen, don't do anything
-           return;
-  }
-  // Ensure that it is a number and stop the keypress
-  if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-      e.preventDefault();
-  }
-});
-
-
